b408aba1cfd01cb1fb7351e15c4a108bde6cbf40
Jian Chen <admin@chensoft.com>
2024-03-24 23:11:48 +0800

    bench: compare between map and sparse array

---
+++ benches/cmp_map_sparse.rs
@@ -0,0 +1,69 @@
+use criterion::*;
+use sparseset::SparseSet;
+use std::collections::HashMap;
+use std::collections::BTreeMap;
+
+fn btreemap(c: &mut Criterion) {
+    let mut obj = BTreeMap::new();
+
+    for i in 0..256usize {
+        obj.insert(i, i);
+    }
+
+    c.bench_function("btreemap", |b| b.iter(|| {
+        for i in 0..256 {
+            assert_eq!(obj.get(&i), Some(&i));
+        }
+    }));
+}
+
+fn hashmap(c: &mut Criterion) {
+    let mut obj = HashMap::new();
+
+    for i in 0..256usize {
+        obj.insert(i, i);
+    }
+
+    c.bench_function("hashmap", |b| b.iter(|| {
+        for i in 0..256 {
+            assert_eq!(obj.get(&i), Some(&i));
+        }
+    }));
+}
+
+fn sparse(c: &mut Criterion) {
+    let mut obj = SparseSet::with_capacity(256);
+
+    for i in 0..256usize {
+        obj.insert(i, i);
+    }
+
+    c.bench_function("sparse", |b| b.iter(|| {
+        for i in 0..256usize {
+            assert_eq!(obj.get(i), Some(&i));
+        }
+    }));
+}
+
+fn array(c: &mut Criterion) {
+    let mut obj = [None; 256];
+
+    for i in 0..256 {
+        obj[i] = Some(i);
+    }
+
+    c.bench_function("array", |b| b.iter(|| {
+        for i in 0..256 {
+            assert_eq!(obj[i].as_ref(), Some(&i));
+        }
+    }));
+}
+
+criterion_group!(
+    benches,
+    btreemap,
+    hashmap,
+    sparse,
+    array,
+);
+criterion_main!(benches);
